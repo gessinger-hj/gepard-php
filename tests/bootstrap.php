3aaffeb7ef68a163ae7b37e9fd28bd9fc264fe73
Paul Gessinger <hello@paulgessinger.com>
2016-09-12 16:04:44 +0200

    don’t bootstrap AspectMock on hhvm

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,11 @@
 <?php
 include __DIR__.'/../vendor/autoload.php'; // composer autoload
 
-$kernel = \AspectMock\Kernel::getInstance();
-$kernel->init([
-    'debug' => true,
-    'includePaths' => [__DIR__.'/../src']
-]);
+if(!defined('HHVM_VERSION')) {
+  $kernel = \AspectMock\Kernel::getInstance();
+  $kernel->init([
+      'debug' => true,
+      'includePaths' => [__DIR__.'/../src']
+  ]);
+}
 ?>
